80ba520fff6d42a8fd2d8c805cb2d3e01fc76895
Tyler Kellen <tyler@sleekcode.net>
2020-04-01 22:26:22 -0400

    stop myself :laughing:

---
+++ arewedoneyet.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+MAX_LINES=500
+CURRENT_LINES=$(cat *.go | grep -v // | grep "\S" | wc -l)
+
+echo $(( ${MAX_LINES} - ${CURRENT_LINES})) lines remain before you must be done.
+
