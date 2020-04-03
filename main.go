a2b20a77f616cd012edea5f59b881fe00f639b86
Tyler Kellen <tyler@sleekcode.net>
2020-04-03 15:18:54 -0400

    restructure cli for testability

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/docopt/docopt-go"
 	"github.com/korovkin/limiter"
 	"github.com/tkellen/memorybox/lib"
@@ -15,10 +16,10 @@ const version = "dev"
 const usage = `memorybox
 
 Usage:
-  $0 [--root=<path> --concurrency=<num> --debug] put local <glob>...
-  $0 [--concurrency=<num> --debug] put s3 <bucket> <glob>...
-  $0 [--root=<path> --debug] get local <glob>
-  $0 [--debug] get s3 <bucket> <glob>
+  $0 [--root=<path> --concurrency=<num> --debug] put local <files>...
+  $0 [--concurrency=<num> --debug] put s3 <bucket> <files>...
+  $0 [--root=<path> --debug] get local <files>
+  $0 [--debug] get s3 <bucket> <files>
 
 Options:
   -c --concurrency=<num>   Max number of concurrent operations [default: 10].
@@ -28,22 +29,23 @@ Options:
   -v --verbose             Show version.
 `
 
+type Command struct {
+	Store memorybox.Store
+	Inputs []string
+	Logger memorybox.Logger
+	Concurrency int
+}
+
 func main() {
-	var command string
 	var concurrency int
 	var store memorybox.Store
 	var err error
+	// Remove timestamp from any log messages.
+	log.SetFlags(0)
 	// Respect what the user named the binary.
 	usage := strings.ReplaceAll(usage, "$0", filepath.Base(os.Args[0]))
 	// Parse command line arguments.
 	cli, _ := docopt.ParseArgs(usage, os.Args[1:], version)
-	// Determine which command we are running.
-	if cli["put"].(bool) {
-		command = "put"
-	}
-	if cli["get"].(bool) {
-		command = "get"
-	}
 	// Prep our backing store.
 	if cli["local"].(bool) {
 		store, err = memorybox.NewLocalStore(cli["--root"].(string))
@@ -55,47 +57,57 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Make informational logging silent by default with a noop function.
-	var logger memorybox.Logger
-	// Enable information logging while in debug mode.
+	// Determine maximum concurrent operations.
+	if s, err := strconv.ParseInt(cli["--concurrency"].(string), 10, 8); err == nil {
+		concurrency = int(s)
+	}
+	// Assemble configuration for running command.
+	command := &Command{
+		Store: store,
+		Inputs: cli["<files>"].([]string),
+		Concurrency: concurrency,
+		Logger: func(string, ...interface{}) {},
+	}
+	// Enable informational logging while in debug mode.
 	if cli["--debug"].(bool) {
-		logger = log.Printf
+		command.Logger = log.Printf
 	}
-	// Remove timestamp from any log messages.
-	log.SetFlags(0)
-	// Determine which files we are operating on.
-	files := cli["<glob>"].([]string)
-
-	// Execute put command.
-	if command == "put" {
-		// Configure maximum concurrent operations.
-		if s, err := strconv.ParseInt(cli["--concurrency"].(string), 10, 8); err == nil {
-			concurrency = int(s)
-		}
-		// Configure concurrency limiting.
-		limit := limiter.NewConcurrencyLimiter(concurrency)
-		// Process every input with a limit on how fast this is performed.
-		for _, input := range files {
-			// Save input in a variable that will be in scope for the closure below.
-			path := input
-			// Execute the puts as fast as we've allowed.
-			limit.Execute(func() {
-				if err := memorybox.Put(path, store, logger); err != nil {
-					log.Printf("bad deal: %s", err)
-				}
-			})
-		}
-		// Wait for all copy operations to complete
-		limit.Wait()
+	// Execute our configuration
+	if cli["put"].(bool) {
+		err = put(command)
 	}
-
-	// Execute get command.
-	if command == "get" {
-		err = memorybox.Get(files[0], store, logger)
+	if cli["get"].(bool) {
+		err = get(command)
 	}
-
 	if err != nil {
 		log.Printf("%s", err)
 		os.Exit(1)
 	}
+}
+
+func put(command *Command) error {
+	var incomplete []error
+	// Configure concurrency limiting.
+	limit := limiter.NewConcurrencyLimiter(command.Concurrency)
+	// Process every input with a limit on how fast this is performed.
+	for _, input := range command.Inputs {
+		// Save input in var that will be in scope for closure below.
+		path := input
+		// Execute puts as fast as we've allowed.
+		limit.Execute(func() {
+			if err := memorybox.Put(path, command.Store, command.Logger); err != nil {
+				incomplete = append(incomplete, err)
+			}
+		})
+	}
+	// Wait for all operations to complete
+	limit.Wait()
+	if len(incomplete) > 0 {
+		return fmt.Errorf("some put operations failed: %s", incomplete)
+	}
+	return nil
+}
+
+func get(command *Command) error {
+	return memorybox.Get(command.Inputs[0], command.Store, command.Logger)
 }
